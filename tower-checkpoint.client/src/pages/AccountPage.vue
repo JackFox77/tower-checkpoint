d2a8c43ad6429e77e86fb98e96e3ad7618b824c2
JackFox77 <jacobfox000@googlemail.com>
2022-08-28 11:37:11 -0600

    init 4

--- tower-checkpoint.client/src/pages/AccountPage.vue
+++ tower-checkpoint.client/src/pages/AccountPage.vue
@@ -4,11 +4,22 @@
     <img class="rounded" :src="account.picture" alt="" />
     <p>{{ account.email }}</p>
   </div>
+
+  <!-- <div class="container">
+    <div class="row">
+      <div class="mx-auto my-3 col-md-10" v-for="p in projects" :key="p.id">
+        <ProjectCard :project="p" />
+      </div>
+    </div>
+  </div> -->
 </template>
 
 <script>
+import { popperOffsets } from '@popperjs/core'
 import { computed } from 'vue'
 import { AppState } from '../AppState'
+import { logger } from '../utils/Logger'
+import Pop from '../utils/Pop'
 export default {
   name: 'Account',
   setup() {
@@ -17,6 +28,27 @@ export default {
     }
   }
 }
+
+
+
+async function getTicketsById() {
+  try {
+
+  } catch (error) {
+    logger.error('[getting tickets]', error)
+    Pop.error(error)
+  }
+}
+
+// // async function getProfileById() {
+//   try {
+//     await profilesService.getProfileById(route.params.profileId)
+//   } catch (error) {
+//     logger.error('[GettingProfile]', error)
+//     Pop.error(error)
+//     router.push({ name: 'Home' })
+//   }
+// }
 </script>
 
 <style scoped>
---
+++ tower-checkpoint.client/src/services/ProfileService.js
@@ -0,0 +1,18 @@
+import { AppState } from "../AppState"
+import { api } from "./AxiosService"
+
+
+
+class ProfileService {
+
+}
+
+
+
+class ProfilesService {
+
+
+    async
+
+}
+export const profileService = new ProfileService()
